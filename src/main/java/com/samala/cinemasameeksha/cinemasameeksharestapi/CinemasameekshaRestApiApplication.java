63b382a8d5b2a07252a8e03dfbdeb0f727103bbe
Reshika Samala <samala.reshika@gmail.com>
2020-02-07 20:23:17 -0800

    Initial commit

---
+++ src/main/java/com/samala/cinemasameeksha/cinemasameeksharestapi/CinemasameekshaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.samala.cinemasameeksha.cinemasameeksharestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemasameekshaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemasameekshaRestApiApplication.class, args);
+	}
+
+}
